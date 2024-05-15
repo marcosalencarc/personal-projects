3b301705e1fe0f4c5cc1895dd0a71d3644a6c3c0
Marcos de Alencar <marcoscarvalho1997@gmail.com>
2024-05-15 14:56:29 -0300

    Init project to studing reactJS

---
+++ rocketseat/react-js/01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,24 @@
+import { Post } from "./Post";
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from './App.module.css'
+import "./global.css";
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post
+            author="Marcos de Alencar"
+            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis quae quis atque repellendus quos quas corporis ut eius velit, dolores natus sapiente porro reprehenderit similique est veritatis eum. Reiciendis, id."
+          />
+          <Post author="Paulo Pedro" content="Lorem ipsum dolor sit." />
+        </main>
+      </div>
+    </>
+  );
+}
